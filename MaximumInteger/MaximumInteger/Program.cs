2e63afc442362da95314ac6616cc3db0e26d21c4
Mansi-Atey <mansiatey31@gmail.com>
2021-03-23 10:38:25 +0530

    [mansi]ADDED: code to find maximum integer

---
+++ MaximumInteger/MaximumInteger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MaximumInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindTheMaximum find = new FindTheMaximum();
+            Console.WriteLine(find.MaximumIntegerNumber(10, 20, 30));
+        }
+    }
+}
