84edf3b4f7c55abd96e3bdd77d055f218ee409e9
Mansi-Atey <mansiatey31@gmail.com>
2021-03-23 11:48:30 +0530

    [mansi]Added: more than three parameters

--- MaximumString/MaximumString/Program.cs
+++ MaximumString/MaximumString/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-namespace MaximumFloat
+namespace MaximumString
 {
     class Program
     {
         static void Main(string[] args)
         {
             FindMaximum find = new FindMaximum();
-            Console.WriteLine(find.MaximumFloatNumber(10.5f, 20.5f, 30.5f));
+            Console.WriteLine(find.MaximumStringNumber("Apple", "Banana", "Carrot"));
         }
     }
 }
---
+++ MaximumString/UnitTestMaximumString/FindMaximum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestMaximumString
+{
+    internal class FindMaximum
+    {
+        internal string MaximumStringNumber(string firstValue, string secondValue, string thirdValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
